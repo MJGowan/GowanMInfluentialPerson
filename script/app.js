755073988c482ef86450024123d00304e6ff9ba4
Madeline <madelinejgowan@gmail.com>
2023-01-31 15:56:40 -0800

    1/31/2023 3:56PM

---
+++ script/app.js
@@ -0,0 +1,13 @@
+let fianceeBtn = document.getElementById("fianceeBtn");
+let teacherBtn = document.getElementById("teacherBtn");
+let badassBtn = document.getElementById("badassBtn");
+
+fianceeBtn.addEventListener('click', function(){
+    window.open('../pages/pageOne.html')
+})
+teacherBtn.addEventListener('click', function(){
+    window.open('../pages/pageTwo.html')
+})
+badassBtn.addEventListener('click', function(){
+    window.open('../pages/pageThree.html')
+})
